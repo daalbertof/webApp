b844df565969de025a48c52a8c85f3986b9a7a00
daalbertof <159089185+daalbertof@users.noreply.github.com>
2026-04-15 18:51:41 -0700

    mas

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function getAllRecords() {
-  let getResultElement = document.getElementById("daniela");
+  let getResultElement = document.getElementById("artists");
 
   const options = {
     method: "GET",
@@ -20,15 +20,71 @@ async function getAllRecords() {
       for (let i = 0; i < data.records.length; i++) {
         let artistPhoto = data.records[i].fields["ArtistPhoto"]; // here we are getting column values
         let name = data.records[i].fields["Name"]; //here we are using the Field ID to fecth the name property
-        let about = data.records[i].fields["About"];
+        let song = data.records[i].fields["Song"];
+        let genre = data.records[i].fields["Genre"];
+
+
+
+
 
         newHtml += `
         
-         <div class="col-xl-4 cardImageText">
+        <div class="col cardImageText p-5">
         <div>${name}</div>
+        <div>${song}</div>
+        <div>${genre}</div>
         <img src="${artistPhoto[0].url}"></img>
-        <div>${about}</div>
         </div>
+
+
+    
+        
+        `;
+      }
+
+      getResultElement.innerHTML = newHtml;
+    });
+}
+
+async function getAllRecords(id) {
+  let getResultElement = document.getElementById("artists");
+
+  const options = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer patSjsYftN8dHvuLD.2a252c2c4acbf7903434fe61f13674cc5ed50e7c6bcc37d8bcb3e462cc52c3f2`,
+    },
+  };
+
+  await fetch(`https://api.airtable.com/v0/appUpsniSDwFioCAm/Table/${id}`, options)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data); // response is an object / .records array
+
+      getResultElement.innerHTML = ""; // clear brews
+
+      let newHtml = "";
+
+      for (let i = 0; i < data.records.length; i++) {
+        let artistPhoto = data.records[i].fields["ArtistPhoto"]; // here we are getting column values
+        let name = data.records[i].fields["Name"]; //here we are using the Field ID to fecth the name property
+        let song = data.records[i].fields["Song"];
+        let genre = data.records[i].fields["Genre"];
+
+
+
+
+
+        newHtml += `
+        
+        <div class="col cardImageText p-5">
+        <div>${name}</div>
+        <div>${song}</div>
+        <div>${genre}</div>
+        <img src="${artistPhoto[0].url}"></img>
+        </div>
+
+
     
         
         `;
