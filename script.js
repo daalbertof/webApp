3daea232b0da2177716a0f7c28d4bbb71fe9abca
daalbertof <159089185+daalbertof@users.noreply.github.com>
2026-04-22 18:53:05 -0700

    eso

--- script.js
+++ script.js
@@ -27,15 +27,15 @@ async function getAllRecords() {
         newHtml += `
         
         <div class="col cardImageText p-3 song-card center">
-        <a href = "details.html?id=${data.records[i].id}">${
-          album ? `<img src="${album[0].url}" class="artists-img"></img>` 
-          : ``
+        <a href = "index.html?id=${data.records[i].id}">${
+          album
+            ? `<img class="artists-img" src="${album[0].url}" ></img>` // Add album ALT!!!!!
+            : ``
         }
-                </a>
-
+        </a>
         </div>
       
-        <div class="info">
+        <div class="info dm-sans">
         <div>${name}</div>
         <div>${song}</div>
         <div>${genre}</div>
@@ -51,9 +51,8 @@ async function getAllRecords() {
     });
 }
 
-
- async function getOneRecord(id) {
-  let getResultElement = document.getElementById("artists");
+async function getOneRecord(id) {
+  let musicResultElement = document.getElementById("artists");
 
   const options = {
     method: "GET",
@@ -62,37 +61,55 @@ async function getAllRecords() {
     },
   };
 
-  await fetch(`https://api.airtable.com/v0/appUpsniSDwFioCAm/Table/${id}`, options)
+  await fetch(
+    `https://api.airtable.com/v0/appUpsniSDwFioCAm/Table/${id}`,
+    options,
+  )
     .then((response) => response.json())
     .then((data) => {
       console.log(data); // response is an object / .records array
 
-      getResultElement.innerHTML = ""; // clear brews
+      let artistPhoto = data.fields["ArtistPhoto"]; // here we are getting column values
+      let name = data.fields["Name"]; //here we are using the Field ID to fecth the name property
+      let song = data.fields["Song"];
+      let genre = data.fields["Genre"];
 
-      let newHtml = "";
-
-      for (let i = 0; i < data.records.length; i++) {
-        let artistPhoto = data.records[i].fields["ArtistPhoto"]; // here we are getting column values
-        let name = data.records[i].fields["Name"]; //here we are using the Field ID to fecth the name property
-        let song = data.records[i].fields["Song"];
-        let genre = data.records[i].fields["Genre"];
-
-
-        newHtml += `
-        
-        <div class="col cardImageText p-5">
+      let newHtml = `
+        <div class="col cardImageText artist-name center p-5">
         <div>${name}</div>
         <div>${song}</div>
         <div>${genre}</div>
-        <img src="${artistPhoto[0].url}"></img>
         </div>
+
+        <div class="rol  p-5 artists-background ">
+        ${
+          artistPhoto
+            ? `<img class="artists-img2" src="${artistPhoto[0].url}">`
+            : ``
+        }
+        </div>
+
 
         
         `;
-      }
+        
 
-      getResultElement.innerHTML = newHtml;
+      musicResultElement.innerHTML = newHtml;
     });
 }
 
-getAllRecords();
+// look up window.location.search and split, so this would take
+// https://dmspr2021-airtable-app.glitch.me/index.html?id=receHhOzntTGZ44I5
+// and look at the ?id=receHhOzntTGZ44I5 part, then split that into an array
+// ["?id=", "receHhOzntTGZ44I5"] and then we only choose the second one
+let idParams = window.location.search.split("?id=");
+if (idParams.length >= 2) {
+  // has at least ["?id=", "OUR ID"]
+  // call function for the dropdown menu
+  getOneRecord(idParams[1]); // create detail view HTML w/ our id
+} else {
+  // call function for the dropdown menu
+  getAllRecords(); // no id given, fetch summaries
+}
+
+//getAllRecords();
